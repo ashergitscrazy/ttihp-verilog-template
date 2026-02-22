58f5e63f0a9f947bdede02639ab6c32ba1d1f712
ajf273 <ajf273@cornell.edu>
2026-02-22 14:44:13 -0500

    control test

--- src/project.v
+++ src/project.v
@@ -16,87 +16,8 @@ module tt_um_ashergitscrazy (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  parameter IDLE = 2'b00;
-  parameter RUN = 2'b01;
-  parameter DONE = 2'b10;
+  assign uo_out = ui_in + uio_in;
 
-
-  reg [1:0] state, next_state;
-
-  reg [7:0] remainder, remainder_next;
-  reg [7:0] root, root_next;
-  reg [1:0] counter, counter_next;
-
-  reg [4:0] test_num;
-
-  assign uo_out = root;
-  
-  always @(posedge clk) begin
-
-    if (!rst_n) begin
-      state <= IDLE;
-      remainder <= 0;
-      root <= 0;
-      counter <= 0;
-    end else begin
-      state <= next_state;
-      remainder <= remainder_next;
-      root <= root_next;
-      counter <= counter_next;
-    end
-
-  end
-
-  always @(*) begin
-
-    next_state = state;
-    remainder_next = remainder;
-    root_next = root;
-    counter_next = counter;
-
-    case(state)
-
-      IDLE: begin
-        if (ena) begin
-          remainder_next = 0;
-          root_next = ui_in;
-          counter_next = 0;
-          next_state = RUN;
-        end
-      end
-
-      RUN: begin
-        // This changes remainder to be remainder with leading digit pair of input appended
-        remainder_next = (remainder << 2) | root[7:6];
-        // Prepares next two bits for following iteration
-        root_next = root << 2;
-        // Prepares 2R + 1, this multiplies root by 2 and changes LSB to 1
-        test_num = (root << 1) | 1;
-
-        // Condition: remainder - test_num is non-negative, so subtraction is allowed
-        if (remainder_next >= test_num) begin
-            remainder_next = remainder_next - test_num;
-            root = (root << 1) | 1;
-        end
-        
-        // Result would be negative, so multiplying root by 2 again
-        else begin
-          root = (root << 1);
-        end
-
-        counter_next = counter + 1;
-        if (counter == 3) next_state = DONE;
-      end
-
-      DONE: begin
-        next_state = IDLE;
-      end
-      
-
-    endcase
-
-  end
-
-  wire _unused = &{uio_in, uio_out, uio_oe, 1'b0};
+  wire _unused = &{ena, uio_out, uio_oe, clk, rst_n, 1'b0};
 
 endmodule
